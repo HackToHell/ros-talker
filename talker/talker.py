1b32d5e38f4b34b886372be8b5d1934095085bab
gowtham <hacktohell@yahoo.com>
2017-12-21 12:51:40 +0900

    Get the topic fromt the ros parameters

--- talker/talker.py
+++ talker/talker.py
@@ -4,7 +4,11 @@ import rospy
 from std_msgs.msg import String
 
 def talker():
-	pub = rospy.Publisher('telemetry', String, queue_size=10)
+	if rospy.has_param('topic_name'):
+		topicname=rospy.get_param('topic_name')
+	else:
+		topicname='telemetry'
+	pub = rospy.Publisher(topicname, String, queue_size=10)
 	rospy.init_node('talker', anonymous=True)
 	rate = rospy.Rate(10)
 	while not rospy.is_shutdown():
